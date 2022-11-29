b59b21533b227898abd259c5da5eafa7b0ede4d1
Liam Baisley <liambaisley@gmail.com>
2022-11-29 20:30:38 +0200

    basics and messing around with leveldb

--- main.go
+++ main.go
@@ -10,13 +10,36 @@ var db *leveldb.DB
 
 func main() {
 	fmt.Println("MiniQ is running...")
-	db = GetConnection()
+	var err error
+	db, err = leveldb.OpenFile("./testDB", nil)
+	if err != nil {
+		fmt.Printf("there was an error")
+		panic("panicking")
+	}
 	defer db.Close()
+
 	var testKey string = "hello"
 	var testValue string = "world"
 
 	fmt.Println("Adding a record")
 	Add([]byte(testKey), []byte(testValue))
+	Add([]byte("D"), []byte(testValue))
+	Add([]byte("B"), []byte(testValue))
+	Add([]byte("F"), []byte(testValue))
+	Add([]byte("Z"), []byte(testValue))
+	Add([]byte("1"), []byte(testValue))
+	Add([]byte("7"), []byte(testValue))
+	Add([]byte("A"), []byte(testValue))
+
+	iter := db.NewIterator(nil, nil)
+	for iter.Next() {
+		// Remember that the contents of the returned slice should not be modified, and
+		// only valid until the next call to Next.
+		key := iter.Key()
+		value := iter.Value()
+		fmt.Printf(string(key) + " " + string(value) + "\n")
+	}
+	iter.Release()
 
 	fmt.Println("trying to retrieve record")
 	var data = Get(testKey)
@@ -24,26 +47,18 @@ func main() {
 	fmt.Println(string(data))
 }
 
-func Get(key string) []byte {
-	data, err := db.Get([]byte(key), nil)
-	if err != nil {
-		panic(err)
-	}
+func Get(key string) string {
+	data, _ := db.Get([]byte(key), nil)
 
-	return data
+	return string(data)
 }
 
 func Add(key []byte, val []byte) bool {
-	err := db.Put([]byte("key"), []byte("value"), nil)
+	err := db.Put(key, val, nil)
 
 	if err != nil {
 		panic(err)
 	}
 
 	return true
-}
-
-func GetConnection() *leveldb.DB {
-	db, _ := leveldb.OpenFile("./", nil)
-	return db
 }
