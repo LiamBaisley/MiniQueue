8eabea0a94eb4496f83a57e19758e01d497d4e79
Liam Baisley <liambaisley@gmail.com>
2022-12-29 09:35:49 +0200

    removed config.json

--- go.mod
+++ go.mod
@@ -3,8 +3,13 @@ module go/miniQ
 go 1.19
 
 require (
+	github.com/gin-gonic/gin v1.8.2
+	github.com/syndtr/goleveldb v1.0.0
+	golang.org/x/crypto v0.4.0
+)
+
+require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
@@ -16,10 +21,8 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
-	github.com/syndtr/goleveldb v1.0.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/crypto v0.4.0 // indirect
-	golang.org/x/net v0.3.0 // indirect
+	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
--- main.go
+++ main.go
@@ -60,9 +60,9 @@ func main() {
 	r := gin.Default()
 
 	//Returns the next message in the Queue
-	r.GET("/getNextMessage", getMessageHandler)
+	r.GET("/message", getMessageHandler)
 	//Adds a new message to the queue
-	r.POST("/postMessage", AddMessageHandler)
+	r.POST("/message", AddMessageHandler)
 	//Confirm consumption of a message before deletion
 	r.POST("/confirm", ConfirmConsumptionHandler)
 
@@ -160,39 +160,4 @@ func ConfirmConsumptionHandler(c *gin.Context) {
 			"message": "Unauthorized.",
 		})
 	}
-}
-
-// Uses the first key if there are no messages in the queue, otherwise incrememts and returns the last key in the queue.
-func GenerateKey() string {
-	iter := db.NewIterator(nil, nil)
-	first := iter.First()
-	var newKey string
-
-	if !first {
-		return firstKey
-	} else {
-		iter.Last()
-		currKey := iter.Key()
-		newKey = IncrementKey(currKey, currKey[len(currKey)-1], len(currKey)-1)
-	}
-
-	return newKey
-}
-
-// Recursively goes through the characters in the key to determine which to increment.
-func IncrementKey(key []byte, currentByte byte, index int) string {
-	if index == 0 && int(currentByte) == 122 {
-		//Considering we have such a large number of possible keys, we should never get here.
-		//But we handle it just in case.
-		panic("Out of keys exception")
-	}
-
-	if int(currentByte) == 122 {
-		key[index] = byte(97)
-		IncrementKey(key, key[index-1], index-1)
-	} else {
-		key[index]++
-	}
-
-	return string(key)
 }
